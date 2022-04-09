1740a4903d59039330336656703c66acdf4d40f9
adeola <67931373+adeola-ak@users.noreply.github.com>
2022-04-08 23:31:21 -0400

    background video with text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import HomeNav from "./HomeNav/HomeNav";
+import { FaArtstation } from "react-icons/fa";
+
+export const Volume = () => {
+  return (
+    <div>
+      <FaArtstation />
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <>
+      <div className="App">
+        <HomeNav />
+        <div class="video-wrapper">
+          <video preload="auto" autoPlay loop muted playsInline data-object-fit>
+            <source
+              src="http://www.jsjr.design/danny/video/video.mp4"
+              type="video/webm"
+            ></source>
+          </video>
+          <div className="page-title">
+            <p className="title">Photographer / Videographer</p>
+            <span className="purpose">
+              DANNY<br></br> DIGITAL<br></br>PORTFOLIO
+            </span>
+          </div>
+          <div className="test">
+            <FaArtstation />
+          </div>{" "}
+        </div>
+      </div>
+
+      {/* <div class="works-filter text-center" role="tablist">
+        <a
+          href="/"
+          class="filter active"
+          role="tab"
+          aria-selected="true"
+          data-filter="*"
+        >
+          All works
+        </a>
+        <a
+          href="#branding"
+          class="filter"
+          role="tab"
+          aria-selected="false"
+          data-filter=".musicvideo"
+        >
+          Videos
+        </a>
+        <a
+          href="#design"
+          class="filter"
+          role="tab"
+          aria-selected="false"
+          data-filter=".interview"
+        >
+          Photography
+        </a>
+      </div> */}
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,8 +77,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
+      </header> */}
+    </>
   );
 }
 
---
+++ src/HomeNav/HomeNav.js
@@ -0,0 +1,18 @@
+import "./HomeNav.css";
+
+const HomeNav = () => {
+  return (
+    <div className="nav">
+      <div className="logo">logo</div>
+      <div className="nav-links">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default HomeNav;
